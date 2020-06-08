f5ff7423f97673bd5ab88c5bf7070855cd24491a
mokasritus <66365291+mokasritus@users.noreply.github.com>
2020-06-08 22:21:54 +0200

    Intro to jQuery

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-$('#first').text('Macrotis');
-$('#second').css("border","5px solid blue");
-$('p').css("background", "yellow");
+let color = "purple";
+let number = 10;
+let word = "cool";
+
+if (color=="purple") {
+  $('.first').css("background", "purple");  
+} 
+
+if (number>100) {
+    $('.second').text("whoah, that's a big number");    
+} else {
+    $('.second').text("just a regular number, please");
+}
+
+if (word=="cool") {
+    $('.third').text("Power of DOM");
+} else {
+    $('.fourth').text("Power of DOM");
+}
